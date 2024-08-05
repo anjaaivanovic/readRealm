e36dd24d8336a2748873705272cf5bf1091cc13f
anja.ivanovic <anja.ivanovic02kg@gmail.com>
2024-08-05 23:38:43 +0200

    Project Structure
    Structured the project, added ConfigProvider, ServiceInitializer, JwtHelper, configured Program.cs (CORS, JWT, Swagger)

---
+++ Initializer/ServiceInitializer.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ReadRealmBackend.Common
+{
+    public class ServiceInitializer
+    {
+        public void Initialize(IServiceCollection services)
+        {
+            InitializeDAL(services);
+            InitializeBL(services);
+            InitializeOther(services);
+        }
+
+        public void InitializeDAL(IServiceCollection services)
+        {
+            
+        }
+
+        public void InitializeBL(IServiceCollection services)
+        {
+
+        }
+
+        public void InitializeOther(IServiceCollection services)
+        {
+            services.AddSingleton<ConfigProvider>();
+            services.AddSingleton<JwtHelper>();
+        }
+    }
+}
---
+++ ReadRealmBackend/Program.cs
@@ -0,0 +1,101 @@
+using ReadRealmBackend.Common;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var serviceInitializer = new ServiceInitializer();
+serviceInitializer.Initialize(builder.Services);
+
+#region Cors
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin", policyBuilder =>
+    {
+        var allowedOrigins = builder.Configuration["Cors:AllowedOrigins"].Split(',');
+        policyBuilder.WithOrigins(allowedOrigins)
+                     .AllowAnyHeader()
+                     .AllowAnyMethod();
+    });
+});
+
+#endregion
+
+#region Jwt
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    });
+
+#endregion
+
+#region Swagger
+
+builder.Services.AddSwaggerGen(option =>
+{
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
+
+#endregion
+
+var app = builder.Build();
+
+app.UseCors();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
