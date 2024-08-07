b8caf9690c8e7717bd3cd90fabb1a6d21669e3da
anja.ivanovic <anja.ivanovic02kg@gmail.com>
2024-08-07 21:39:38 +0200

    BaseDAL

---
+++ ReadRealmBackend.DAL/BaseDAL/BaseDAL.cs
@@ -0,0 +1,73 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using ReadRealmBackend.Models.Context;
+
+namespace ReadRealmBackend.DAL.BaseDAL
+{
+    public class BaseDAL<TEntity> : IBaseDAL<TEntity> where TEntity : class
+    {
+        private readonly ReadRealmContext _context;
+        private readonly DbSet<TEntity> _set;
+
+        public BaseDAL(ReadRealmContext context)
+        {
+            _context = context;
+            _set = _context.Set<TEntity>();
+        }
+
+        #region SaveChanges
+
+        public async Task<bool> SaveAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        #endregion
+
+        #region Get
+
+        public async Task<TEntity?> GetOneAsync(int id)
+        {
+            return await _set.FindAsync(id);
+        }
+
+        #endregion
+
+        #region Insert
+
+        public async Task InsertOneAsync(TEntity entity)
+        {
+            await _set.AddAsync(entity);
+        }
+
+        public async Task InsertMultipleAsync(List<TEntity> entities)
+        {
+            await _set.AddRangeAsync(entities);
+        }
+
+        #endregion
+
+        #region Update
+
+        public void UpdateOne(TEntity entity)
+        {
+            _set.Update(entity);
+        }
+
+        #endregion
+
+        #region Delete
+
+        public void DeleteOne(TEntity entity)
+        {
+            _set.Remove(entity);
+        }
+
+        public void DeleteMultiple(List<TEntity> entities)
+        {
+            _set.RemoveRange(entities);
+        }
+
+        #endregion
+    }
+}
---
+++ ReadRealmBackend.DAL/BaseDAL/IBaseDAL.cs
@@ -0,0 +1,13 @@
+﻿namespace ReadRealmBackend.DAL.BaseDAL
+{
+    public interface IBaseDAL<TEntity>
+    {
+        Task<TEntity?> GetOneAsync(int id);
+        Task InsertOneAsync(TEntity entity);
+        Task InsertMultipleAsync(List<TEntity> entities);
+        void UpdateOne(TEntity entity);
+        void DeleteOne(TEntity entity);
+        void DeleteMultiple(List<TEntity> entities);
+        Task<bool> SaveAsync();
+    }
+}
